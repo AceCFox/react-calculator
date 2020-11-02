e3b2cfef61166ebbe9cdd1b267b345ab2ac3fc18
Ace Fox <afox09@gmail.com>
2020-11-02 14:21:14 -0600

    interval updates

--- src/modules/ItemList.js
+++ src/modules/ItemList.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {ListItem, List, ListItemText, Paper,} from '@material-ui/core';
 import {useSelector} from 'react-redux'
 import { useDispatch } from 'react-redux'
 
 function ItemList() {
   const history = useSelector(state => state.calculations);
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+        dispatch({type: 'GET_CALC'});
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div >
