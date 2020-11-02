b3771dcee8b4ea13b40327cb34ea2aa1defe530e
Ace Fox <afox09@gmail.com>
2020-11-02 10:58:52 -0600

    updated db

--- database.sql
+++ database.sql
@@ -1,35 +1,6 @@
--- Create a postgresql database called "bread-box" and run the following queries in order:
+-- Create a postgresql database called "react-calculator" and run the following queries in order:
 
-CREATE TABLE "item"(
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(50),
-    "tag_id" INT REFERENCES "tag",
-    "refresh period" INT,
-    "needed" BOOLEAN default true
-);
-
-CREATE TABLE "store"(
+CREATE TABLE "calculation" (
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR (50),
-	"status" VARCHAR (50)
-);
-
-CREATE TABLE "store-item"(
-	"id" SERIAL PRIMARY KEY,
-	"store_id" INT REFERENCES "store",
-	"item_id" INT REFERENCES "item"
-);
-
-CREATE TABLE "tag"(
-    "id" SERIAL PRIMARY KEY,
-    "color" VARCHAR(20),
-    "label" VARCHAR(50)
-);
-
-INSERT INTO "store"(
-"name", "status") VALUES
-    ('Aldi', 'favorite'),
-    ('Target', null),
-    ('Shuang Hur', null),
-    ('Wedge', null),
-    ('Cub', null);
+	"calculation" VARCHAR (1000)
+	);
